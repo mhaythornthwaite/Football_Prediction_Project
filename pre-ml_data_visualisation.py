9a500bfadb0b852c8f699a0b384615e4990ffd68
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-04-27 20:30:46 +0100

    Data Visualisation v2

--- pre-ml_data_visualisation.py
+++ pre-ml_data_visualisation.py
@@ -26,123 +26,353 @@ with open('2019_prem_generated_clean/2019_prem_df_for_ml_5.txt', 'rb') as myFile
 with open('2019_prem_generated_clean/2019_prem_df_for_ml_10.txt', 'rb') as myFile:
     df_ml_10 = pickle.load(myFile)
 
-normal_dist = np.random.normal(40, 10, 1000)
-normal_dist = np.round(normal_dist, 2)
+#standard variables for every figure
+colourbar = 'inferno'
+target_goals_min = 0
+target_goals_max = 4
 
-#subplots option 1 (recommended)
-fig, ((ax1, ax2, ax3, ax4, ax5, ax6, ax7), (ax8, ax9, ax10, ax11, ax12, ax13, ax14)) = plt.subplots(nrows = 2, ncols = 7, figsize = (19, 6))
-ax1.scatter(df_ml_10['Team Av Shots'], df_ml_10['Team Goal Target']);
-ax2.scatter(df_ml_10['Team Av Shots Inside Box'], df_ml_10['Team Goal Target']);
-ax3.scatter(df_ml_10['Team Av Fouls'], df_ml_10['Team Goal Target']);
-ax4.scatter(df_ml_10['Team Av Corners'], df_ml_10['Team Goal Target']);
-ax5.scatter(df_ml_10['Team Av Possession'], df_ml_10['Team Goal Target']);
-ax6.scatter(df_ml_10['Team Av Pass Accuracy'], df_ml_10['Team Goal Target']);
-ax7.scatter(df_ml_10['Team Av Goals'], df_ml_10['Team Goal Target']);
-ax8.scatter(df_ml_10['Opponent Av Shots'], df_ml_10['Team Goal Target']);
-ax9.scatter(df_ml_10['Opponent Av Shots Inside Box'], df_ml_10['Team Goal Target']);
-ax10.scatter(df_ml_10['Opponent Av Fouls'], df_ml_10['Team Goal Target']);
-ax11.scatter(df_ml_10['Opponent Av Corners'], df_ml_10['Team Goal Target']);
-ax12.scatter(df_ml_10['Opponent Av Possession'], df_ml_10['Team Goal Target']);
-ax13.scatter(df_ml_10['Opponent Av Goals'], df_ml_10['Team Goal Target']);
-ax14.scatter(df_ml_10['Opponent Av Pass Accuracy'], df_ml_10['Team Goal Target']);
+#----------------------------------- FIGURE 1 ---------------------------------
 
-fig.tight_layout(pad=2.0, h_pad=2)
-
-
-
-#figure 2
+#figure 1 - setting up the wrapper
 fig, ((ax1, ax2, ax3),(ax4, ax5, ax6)) = plt.subplots(ncols=3,
                                                       nrows=2,
                                                       figsize=(18,12))
+
+fig.suptitle('Data Averaged Over 10 Games - Target = Team Goals', y=0.99, fontsize=16, fontweight='bold');
 
 #plotting the 6 figures
 scat1 = ax1.scatter(df_ml_10['Team Av Shots'], 
                    df_ml_10['Opponent Av Shots'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 scat2 = ax2.scatter(df_ml_10['Team Av Shots Inside Box'], 
                    df_ml_10['Opponent Av Shots Inside Box'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 scat3 = ax3.scatter(df_ml_10['Team Av Fouls'], 
                    df_ml_10['Opponent Av Fouls'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 scat4 = ax4.scatter(df_ml_10['Team Av Corners'], 
                    df_ml_10['Opponent Av Corners'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 
 scat5 = ax5.scatter(df_ml_10['Team Av Pass Accuracy'], 
                    df_ml_10['Opponent Av Pass Accuracy'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 scat6 = ax6.scatter(df_ml_10['Team Av Goals'], 
                    df_ml_10['Opponent Av Goals'], 
                    c=df_ml_10['Team Goal Target'],
-                   cmap='inferno',
-                   vmin = 0,
-                   vmax = 4);
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 #setting axis and legend for all 6 figures
+fig.tight_layout(pad=6)
+
+ax1.set(xlabel='Team Average Shots',
+        ylabel='Opponent Average Shots');
+
+ax2.set(xlabel='Team Average Shots Inside Box',
+        ylabel='Opponent Average Shots Inside Box');
+
+ax3.set(xlabel='Team Average Fouls',
+        ylabel='Opponent Average Fouls');
+
+ax4.set(xlabel='Team Average Corners',
+        ylabel='Opponent Average Corners');
+
+ax5.set(xlabel='Team Average Pass Accuracy %',
+        ylabel='Opponent Average Pass Accuracy %');
+
+ax6.set(xlabel='Team Average Goals',
+        ylabel='Opponent Average Goals');
+
+ax_iter = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+for ax in ax_iter:
+    ax.legend(*scat2.legend_elements(), title='Target \n Team \n Goals', loc='upper right', fontsize='small');
+    ax.set_axisbelow(True)
+    ax.grid(color='xkcd:light grey')
+
+#saving figure
+fig.savefig('figures/average_10_games_team_target_goals.png')
+
+
+#----------------------------------- FIGURE 2 ---------------------------------
+
+#figure 2 - setting up the wrapper
+fig, ((ax1, ax2, ax3),(ax4, ax5, ax6)) = plt.subplots(ncols=3,
+                                                      nrows=2,
+                                                      figsize=(18,12))
+
+fig.suptitle('Data Averaged Over 10 Games - Target = Opponent Goals', y=0.99, fontsize=16, fontweight='bold');
+
+#plotting the 6 figures
+scat1 = ax1.scatter(df_ml_10['Team Av Shots'], 
+                   df_ml_10['Opponent Av Shots'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat2 = ax2.scatter(df_ml_10['Team Av Shots Inside Box'], 
+                   df_ml_10['Opponent Av Shots Inside Box'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat3 = ax3.scatter(df_ml_10['Team Av Fouls'], 
+                   df_ml_10['Opponent Av Fouls'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat4 = ax4.scatter(df_ml_10['Team Av Corners'], 
+                   df_ml_10['Opponent Av Corners'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+
+scat5 = ax5.scatter(df_ml_10['Team Av Pass Accuracy'], 
+                   df_ml_10['Opponent Av Pass Accuracy'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat6 = ax6.scatter(df_ml_10['Team Av Goals'], 
+                   df_ml_10['Opponent Av Goals'], 
+                   c=df_ml_10['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+#setting axis and legend for all 6 figures
+fig.tight_layout(pad=6)
+
+ax1.set(xlabel='Team Average Shots',
+        ylabel='Opponent Average Shots');
+
+ax2.set(xlabel='Team Average Shots Inside Box',
+        ylabel='Opponent Average Shots Inside Box');
+
+ax3.set(xlabel='Team Average Fouls',
+        ylabel='Opponent Average Fouls');
+
+ax4.set(xlabel='Team Average Corners',
+        ylabel='Opponent Average Corners');
+
+ax5.set(xlabel='Team Average Pass Accuracy %',
+        ylabel='Opponent Average Pass Accuracy %');
+
+ax6.set(xlabel='Team Average Goals',
+        ylabel='Opponent Average Goals');
+
+ax_iter = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+for ax in ax_iter:
+    ax.legend(*scat2.legend_elements(), title='   Target \n Opponent \n    Goals', loc='upper right', fontsize='small');
+    ax.set_axisbelow(True)
+    ax.grid(color='xkcd:light grey')
+
+#saving figure
+fig.savefig('figures/average_10_games_opponent_target_goals.png')
+
+
+#----------------------------------- FIGURE 3 ---------------------------------
+
+
+#figure 3 - setting up the wrapper
+fig, ((ax1, ax2, ax3),(ax4, ax5, ax6)) = plt.subplots(ncols=3,
+                                                      nrows=2,
+                                                      figsize=(18,12))
+
+fig.suptitle('Data Averaged Over 5 Games - Target = Team Goals', y=0.99, fontsize=16, fontweight='bold');
+
+#plotting the 6 figures
+scat1 = ax1.scatter(df_ml_5['Team Av Shots'], 
+                   df_ml_5['Opponent Av Shots'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat2 = ax2.scatter(df_ml_5['Team Av Shots Inside Box'], 
+                   df_ml_5['Opponent Av Shots Inside Box'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat3 = ax3.scatter(df_ml_5['Team Av Fouls'], 
+                   df_ml_5['Opponent Av Fouls'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat4 = ax4.scatter(df_ml_5['Team Av Corners'], 
+                   df_ml_5['Opponent Av Corners'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+
+scat5 = ax5.scatter(df_ml_5['Team Av Pass Accuracy'], 
+                   df_ml_5['Opponent Av Pass Accuracy'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat6 = ax6.scatter(df_ml_5['Team Av Goals'], 
+                   df_ml_5['Opponent Av Goals'], 
+                   c=df_ml_5['Team Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+#setting axis and legend for all 6 figures
+fig.tight_layout(pad=6)
+
+ax1.set(xlabel='Team Average Shots',
+        ylabel='Opponent Average Shots');
+
+ax2.set(xlabel='Team Average Shots Inside Box',
+        ylabel='Opponent Average Shots Inside Box');
+
+ax3.set(xlabel='Team Average Fouls',
+        ylabel='Opponent Average Fouls');
+
+ax4.set(xlabel='Team Average Corners',
+        ylabel='Opponent Average Corners');
+
+ax5.set(xlabel='Team Average Pass Accuracy %',
+        ylabel='Opponent Average Pass Accuracy %');
+
+ax6.set(xlabel='Team Average Goals',
+        ylabel='Opponent Average Goals');
+
+ax_iter = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+for ax in ax_iter:
+    ax.legend(*scat2.legend_elements(), title='Target \n Team \n Goals', loc='upper right', fontsize='small');
+    ax.set_axisbelow(True)
+    ax.grid(color='xkcd:light grey')
+
+#saving figure
+fig.savefig('figures/average_5_games_team_target_goals.png')
 
 
 
+#----------------------------------- FIGURE 4 ---------------------------------
 
-df_ml_10['Team Av Shots']
-df_ml_10['Team Av Shots Inside Box']
-df_ml_10['Team Av Fouls']
-df_ml_10['Team Av Corners']
-df_ml_10['Team Av Possession']
-df_ml_10['Team Av Pass Accuracy']
-df_ml_10['Team Av Goals']
+#figure 4 - setting up the wrapper
+fig, ((ax1, ax2, ax3),(ax4, ax5, ax6)) = plt.subplots(ncols=3,
+                                                      nrows=2,
+                                                      figsize=(18,12))
 
-df_ml_10['Opponent Av Shots']
-df_ml_10['Opponent Av Shots Inside Box']
-df_ml_10['Opponent Av Fouls']
-df_ml_10['Opponent Av Corners']
-df_ml_10['Opponent Av Possession']
-df_ml_10['Opponent Av Goals']
-df_ml_10['Opponent Av Pass Accuracy']
+fig.suptitle('Data Averaged Over 5 Games - Target = Opponent Goals', y=0.99, fontsize=16, fontweight='bold');
 
-df_ml_10['Team Goal Target']
-df_ml_10['Opponent Goal Target']
+#plotting the 6 figures
+scat1 = ax1.scatter(df_ml_5['Team Av Shots'], 
+                   df_ml_5['Opponent Av Shots'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat2 = ax2.scatter(df_ml_5['Team Av Shots Inside Box'], 
+                   df_ml_5['Opponent Av Shots Inside Box'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat3 = ax3.scatter(df_ml_5['Team Av Fouls'], 
+                   df_ml_5['Opponent Av Fouls'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat4 = ax4.scatter(df_ml_5['Team Av Corners'], 
+                   df_ml_5['Opponent Av Corners'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
 
 
+scat5 = ax5.scatter(df_ml_5['Team Av Pass Accuracy'], 
+                   df_ml_5['Opponent Av Pass Accuracy'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+scat6 = ax6.scatter(df_ml_5['Team Av Goals'], 
+                   df_ml_5['Opponent Av Goals'], 
+                   c=df_ml_5['Opponent Goal Target'],
+                   cmap = colourbar,
+                   vmin = target_goals_min,
+                   vmax = target_goals_max);
+
+#setting axis and legend for all 6 figures
+fig.tight_layout(pad=6)
+
+ax1.set(xlabel='Team Average Shots',
+        ylabel='Opponent Average Shots');
+
+ax2.set(xlabel='Team Average Shots Inside Box',
+        ylabel='Opponent Average Shots Inside Box');
+
+ax3.set(xlabel='Team Average Fouls',
+        ylabel='Opponent Average Fouls');
+
+ax4.set(xlabel='Team Average Corners',
+        ylabel='Opponent Average Corners');
+
+ax5.set(xlabel='Team Average Pass Accuracy %',
+        ylabel='Opponent Average Pass Accuracy %');
+
+ax6.set(xlabel='Team Average Goals',
+        ylabel='Opponent Average Goals');
+
+ax_iter = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+for ax in ax_iter:
+    ax.legend(*scat2.legend_elements(), title='   Target \n Opponent \n    Goals', loc='upper right', fontsize='small');
+    ax.set_axisbelow(True)
+    ax.grid(color='xkcd:light grey')
+
+#saving figure
+fig.savefig('figures/average_5_games_opponent_target_goals.png')
 
 
-
-df_ml_5['Team Av Shots']
-df_ml_5['Team Av Shots Inside Box']
-df_ml_5['Team Av Fouls']
-df_ml_5['Team Av Corners']
-df_ml_5['Team Av Possession']
-df_ml_5['Team Av Pass Accuracy']
-df_ml_5['Team Av Goals']
-df_ml_5['Opponent Av Shots']
-df_ml_5['Opponent Av Shots Inside Box']
-df_ml_5['Opponent Av Fouls']
-df_ml_5['Opponent Av Corners']
-df_ml_10['Opponent Av Possession']
-df_ml_10['Opponent Av Goals']
-df_ml_10['Opponent Av Pass Accuracy']
-df_ml_10['Team Goal Target']
-df_ml_10['Opponent Goal Target']
 
 
 # ----------------------------------- END -------------------------------------
