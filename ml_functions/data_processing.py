9d0dd5c0e83af95f8b31bdace9e888ab75a2387d
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-06-14 17:12:35 +0100

    Scree Plot Function

--- ml_functions/data_processing.py
+++ ml_functions/data_processing.py
@@ -18,8 +18,6 @@ from sklearn import preprocessing
 
 
 def scale_df(df, scale, unscale):
-    
-    
     '''
     This function will use the preprocessing function in sklearn to rescale each feature to haze zero mean and unit vector (mean=0, variance=1). Output is df instead of np array
 
@@ -55,3 +53,46 @@ def scale_df(df, scale, unscale):
     return final_df
 
         
+
+def scree_plot(pca_percentages):
+    '''
+    Input principle component percentages list and returns scree plot
+
+    Parameters
+    ----------
+    pca_percentages : list
+        principle component percentage variation.
+
+    Returns
+    -------
+    fig : fig
+        bar plot.
+
+    '''
+    
+    #setting up variables
+    n_components = len(pca_percentages)
+    
+    #instantiating figure
+    fig, ax = plt.subplots()
+    
+    #plot bar component
+    ax.bar(list(range(1, n_components+1, 1)), pca_percentages, color='paleturquoise', edgecolor='darkturquoise', zorder=0)
+    
+    #annotating with percentages
+    for p in ax.patches:
+        ax.annotate(f'{round(p.get_height(), 1)}%', (p.get_x() + 0.5, p.get_height() + 0.5))
+    
+    #plot line and points of each principle component
+    ax.plot(list(range(1, n_components+1, 1)), pca_percentages, c='firebrick', zorder=1)
+    ax.scatter(list(range(1, n_components+1, 1)), pca_percentages, c='firebrick', zorder=2)
+    
+    #Plotting details
+    fig.suptitle('PCA Scree Plot', y=0.96, fontsize=16, fontweight='bold');
+    ax.set(xlabel='Principle Components',
+           ylabel='Percentage Variation');
+    ax.set_ylim([0,40])
+    
+    return fig
+
+
