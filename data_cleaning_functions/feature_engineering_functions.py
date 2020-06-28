45dff0b7663d06ea1fc8abe2e758a6a7b5eefc78
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-06-28 14:34:19 +0100

    Feature Engineering - Major Architecture Change

--- data_cleaning_functions/feature_engineering_functions.py
+++ data_cleaning_functions/feature_engineering_functions.py
@@ -194,6 +194,70 @@ def generate_ml_df(games_slide, team_list, team_fixture_id_dict, game_stats, mak
 
 
 
+def mod_df(df):
+    df_sort = df.sort_values('Target Fixture ID')
+    df_sort = df_sort.reset_index(drop=True)
+    
+    df_output = pd.DataFrame({})
+    #creating our desired features
+    df_output['Av Shots Diff'] = df_sort['Team Av Shots'] - df_sort['Opponent Av Shots']
+    df_output['Av Shots Inside Box Diff'] = df_sort['Team Av Shots Inside Box'] - df_sort['Opponent Av Shots Inside Box']
+    df_output['Av Fouls Diff'] = df_sort['Team Av Fouls'] - df_sort['Opponent Av Fouls']
+    df_output['Av Corners Diff'] = df_sort['Team Av Corners'] -df_sort['Opponent Av Corners']
+    df_output['Av Possession Diff'] = df_sort['Team Av Possession'] - df_sort['Opponent Av Possession']
+    df_output['Av Pass Accuracy Diff'] = df_sort['Team Av Pass Accuracy'] - df_sort['Opponent Av Pass Accuracy']
+    df_output['Av Goal Difference'] = df_sort['Team Av Goals'] - df_sort['Opponent Av Goals']
+    df_output['Fixture ID'] = df_sort['Target Fixture ID']
+    df_output['Result Indicator'] = df_sort['Result Indicator']
+    
+    return df_output
+
+
+
+def combining_fixture_id(df):
+    #iterating over each opponent row to add to the previous
+    odd_list = []
+    for x in range(1, len(df)+1, 2):
+        odd_list.append(x)
+    even_list = []
+    for x in range(0, len(df)-1, 2):
+        even_list.append(x)
+        
+    team_df = df.drop(df.index[odd_list])
+    team_df = team_df.reset_index(drop=True)
+    opponent_df = df.drop(df.index[even_list])
+    opponent_df = opponent_df.reset_index(drop=True)
+    
+    df_output = pd.DataFrame({})
+    df_output['Team Av Shots Diff'] = team_df['Av Shots Diff']
+    df_output['Team Av Shots Inside Box Diff'] = team_df['Av Shots Inside Box Diff']
+    df_output['Team Av Fouls Diff'] = team_df['Av Fouls Diff']
+    df_output['Team Av Corners Diff'] = team_df['Av Corners Diff']
+    df_output['Team Av Possession Diff'] = team_df['Av Possession Diff']
+    df_output['Team Av Pass Accuracy Diff'] = team_df['Av Pass Accuracy Diff']
+    df_output['Team Av Goal Diff'] = team_df['Av Goal Difference']
+    df_output['Opponent Av Shots Diff'] = opponent_df['Av Shots Diff']
+    df_output['Opponent Av Shots Inside Box Diff'] = opponent_df['Av Shots Inside Box Diff']
+    df_output['Opponent Av Fouls Diff'] = opponent_df['Av Fouls Diff']
+    df_output['Opponent Av Corners Diff'] = opponent_df['Av Corners Diff']
+    df_output['Opponent Av Possession Diff'] = opponent_df['Av Possession Diff']
+    df_output['Opponent Av Pass Accuracy Diff'] = opponent_df['Av Pass Accuracy Diff']
+    df_output['Opponent Av Goal Diff'] = opponent_df['Av Goal Difference']
+    df_output['Fixture ID'] = team_df['Fixture ID']
+    df_output['Team Result Indicator'] = team_df['Result Indicator']
+    df_output['Opponent Result Indicator'] = opponent_df['Result Indicator']
+    
+    return df_output
+
+
+def creating_ml_df(df):
+    modified_df = mod_df(df)
+    comb_df = combining_fixture_id(modified_df)
+    return comb_df
+
+
+
+
 
 
 
--- feature_engineering.py
+++ feature_engineering.py
@@ -15,6 +15,7 @@ import pandas as pd
 import pickle
 import numpy as np
 from data_cleaning_functions.feature_engineering_functions import generate_ml_df, running_mean
+from data_cleaning_functions.feature_engineering_functions import mod_df, combining_fixture_id, creating_ml_df
 
 #----------------------------- FEATURE ENGINEERING ----------------------------
 
@@ -28,6 +29,7 @@ team_list = []
 for key in game_stats.keys():
     team_list.append(key)
 team_list.sort()
+
 
 #creating a dictionary with the team id as key and fixture id's as values
 team_fixture_id_dict = {}
@@ -45,18 +47,24 @@ for team in team_fixture_id_dict:
     team_fixture_id_dict[team].sort()
 
 
-#we can now iterate over the fixture ID list given a team id key using the dict created above. N.B./ the number of games over which the past data is averaged. A large number will smooth out past performance where as a small number will result in the prediction being heavily reliant on very recent form. This is worth testing the ml model build phase
+#we can now iterate over the fixture ID list given a team id key using the dict created above. N.B./ the number of games over which the past data is averaged. A large number will smooth out past performance where as a small number will result in the prediction being heavily reliant on very recent form. This is worth testing the ml model build phase.
 
+#5 game sliding average.
+df_ml_5 = generate_ml_df(5, team_list, team_fixture_id_dict, game_stats)
 
+#10 game sliding average.
+df_ml_10 = generate_ml_df(10, team_list, team_fixture_id_dict, game_stats)
+    
+    
 #creating and saving the ml dataframe with a 5 game sliding average.
-df_ready_for_ml_5 = generate_ml_df(5, team_list, team_fixture_id_dict, game_stats)
-with open('2019_prem_generated_clean/2019_prem_df_for_ml_5.txt', 'wb') as myFile:
-    pickle.dump(df_ready_for_ml_5, myFile)
+df_for_ml_5_v2 = creating_ml_df(df_ml_5)
+with open('2019_prem_generated_clean/2019_prem_df_for_ml_5_v2.txt', 'wb') as myFile:
+    pickle.dump(df_for_ml_5_v2, myFile)
 
 #creating and saving the ml dataframe with a 10 game sliding average.
-df_ready_for_ml_10 = generate_ml_df(10, team_list, team_fixture_id_dict, game_stats)
-with open('2019_prem_generated_clean/2019_prem_df_for_ml_10.txt', 'wb') as myFile:
-    pickle.dump(df_ready_for_ml_10, myFile)
+df_for_ml_10_v2 = creating_ml_df(df_ml_10)
+with open('2019_prem_generated_clean/2019_prem_df_for_ml_10_v2.txt', 'wb') as myFile:
+    pickle.dump(df_for_ml_10_v2, myFile)
 
 
 # ----------------------------------- END -------------------------------------
