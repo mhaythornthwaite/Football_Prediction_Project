7d3a45979c53f3b8af1c7a9b7170c9a95099535a
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-07-01 20:25:39 +0100

    Server Update

--- web_server/server.py
+++ web_server/server.py
@@ -20,16 +20,92 @@ import pickle
 with open('../predictions/pl_predictions.csv', 'rb') as myFile:
     pl_pred = pickle.load(myFile)
 
+g1_h = pl_pred['Home Team'][0]
+g1_a = pl_pred['Away Team'][0]
+g1_hw = pl_pred['Home Win'][0]
+g1_d = pl_pred['Draw'][0]
+g1_aw = pl_pred['Away Win'][0]
+
+
+g2_h = pl_pred['Home Team'][1]
+g2_a = pl_pred['Away Team'][1]
+g2_hw = pl_pred['Home Win'][1]
+g2_d = pl_pred['Draw'][1]
+g2_aw = pl_pred['Away Win'][1]
+
+
+g3_h = pl_pred['Home Team'][2]
+g3_a = pl_pred['Away Team'][2]
+g3_hw = pl_pred['Home Win'][2]
+g3_d = pl_pred['Draw'][2]
+g3_aw = pl_pred['Away Win'][2]
+
+
+g4_h = pl_pred['Home Team'][3]
+g4_a = pl_pred['Away Team'][3]
+g4_hw = pl_pred['Home Win'][3]
+g4_d = pl_pred['Draw'][3]
+g4_aw = pl_pred['Away Win'][3]
+
+
+g5_h = pl_pred['Home Team'][4]
+g5_a = pl_pred['Away Team'][4]
+g5_hw = pl_pred['Home Win'][4]
+g5_d = pl_pred['Draw'][4]
+g5_aw = pl_pred['Away Win'][4]
+
+
+g6_h = pl_pred['Home Team'][5]
+g6_a = pl_pred['Away Team'][5]
+g6_hw = pl_pred['Home Win'][5]
+g6_d = pl_pred['Draw'][5]
+g6_aw = pl_pred['Away Win'][5]
+
+
+g7_h = pl_pred['Home Team'][6]
+g7_a = pl_pred['Away Team'][6]
+g7_hw = pl_pred['Home Win'][6]
+g7_d = pl_pred['Draw'][6]
+g7_aw = pl_pred['Away Win'][6]
+
+
+g8_h = pl_pred['Home Team'][7]
+g8_a = pl_pred['Away Team'][7]
+g8_hw = pl_pred['Home Win'][7]
+g8_d = pl_pred['Draw'][7]
+g8_aw = pl_pred['Away Win'][7]
+
+
+g9_h = pl_pred['Home Team'][8]
+g9_a = pl_pred['Away Team'][8]
+g9_hw = pl_pred['Home Win'][8]
+g9_d = pl_pred['Draw'][8]
+g9_aw = pl_pred['Away Win'][8]
+
+
+g10_h = pl_pred['Home Team'][9]
+g10_a = pl_pred['Away Team'][9]
+g10_hw = pl_pred['Home Win'][9]
+g10_d = pl_pred['Draw'][9]
+g10_aw = pl_pred['Away Win'][9]
 
 
 @app.route('/')
-def hello_world():
-    g1_h = pl_pred['Home Team'][0]
-    g1_a = pl_pred['Away Team'][0]
-    g1_hw = pl_pred['Home Win'][0]
-    g1_d = pl_pred['Draw'][0]
-    g1_aw = pl_pred['Away Win'][0]
-    return render_template('index.html', g1_h=g1_h, g1_a=g1_a, g1_hw=g1_hw, g1_d=g1_d, g1_aw=g1_aw)
+def pass_game_1():
+    return render_template('index.html', 
+                           g1_h=g1_h, g1_a=g1_a, g1_hw=g1_hw, g1_d=g1_d, g1_aw=g1_aw,
+                           g2_h=g2_h, g2_a=g2_a, g2_hw=g2_hw, g2_d=g2_d, g2_aw=g2_aw,
+                           g3_h=g3_h, g3_a=g3_a, g3_hw=g3_hw, g3_d=g3_d, g3_aw=g3_aw,
+                           g4_h=g4_h, g4_a=g4_a, g4_hw=g4_hw, g4_d=g4_d, g4_aw=g4_aw,
+                           g5_h=g5_h, g5_a=g5_a, g5_hw=g5_hw, g5_d=g5_d, g5_aw=g5_aw,
+                           g6_h=g6_h, g6_a=g6_a, g6_hw=g6_hw, g6_d=g6_d, g6_aw=g6_aw,
+                           g7_h=g7_h, g7_a=g7_a, g7_hw=g7_hw, g7_d=g7_d, g7_aw=g7_aw,
+                           g8_h=g8_h, g8_a=g8_a, g8_hw=g8_hw, g8_d=g8_d, g8_aw=g8_aw,
+                           g9_h=g9_h, g9_a=g9_a, g9_hw=g9_hw, g9_d=g9_d, g9_aw=g9_aw,
+                           g10_h=g10_h, g10_a=g10_a, g10_hw=g10_hw, g10_d=g10_d, g10_aw=g10_aw)
+
+
+
 
 # =============================================================================
 # @app.route('/')
@@ -38,7 +114,7 @@ def hello_world():
 # =============================================================================
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
     
 
