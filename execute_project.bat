a4a829310b7412962096c06b7cd0fcf451d4ddd5
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-07-20 18:43:51 +0100

    Bat File

--- execute_project.bat
+++ execute_project.bat
@@ -1,1 +1,13 @@
-%~dp0\api_data_request.py
+"C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\venv\Scripts\python.exe" "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\api_data_request.py"
+
+"C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\venv\Scripts\python.exe" "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\cleaning_stats_data.py"
+
+"C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\venv\Scripts\python.exe" "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\feature_engineering.py"
+
+cd "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\ml_model_build_random_forest"
+"C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\venv\Scripts\python.exe" "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\ml_model_build_random_forest\random_forest_model_build.py"
+
+cd "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\predictions"
+"C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\venv\Scripts\python.exe" "C:\Users\mhayt\Documents\Software_Developer-Python\Football_Prediction_Project\predictions\predictions.py"
+
+pause
