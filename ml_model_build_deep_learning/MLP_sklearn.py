e1d423cc536ffd77eb2598d230a5a7df704ddc79
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-08-30 13:35:38 +0100

    MLP Final Model

--- ml_model_build_deep_learning/MLP_sklearn.py
+++ ml_model_build_deep_learning/MLP_sklearn.py
@@ -36,15 +36,19 @@ plt.close('all')
 df_5_saved_name = '2019_prem_df_for_ml_5_v2.txt'
 df_10_saved_name = '2019_prem_df_for_ml_10_v2.txt'
 
+grid_search = False
+save_grid_search_fig = False
+
+create_final_model = True
 
 #------------------------------- ML MODEL BUILD -------------------------------
 
 #importing the data and creating the feature dataframe and target series
 
-with open('../prem_clean_fixtures_and_dataframes/2019_prem_df_for_ml_5_v2.txt', 'rb') as myFile:
+with open(f'../prem_clean_fixtures_and_dataframes/{df_5_saved_name}', 'rb') as myFile:
     df_ml_5 = pickle.load(myFile)
 
-with open('../prem_clean_fixtures_and_dataframes/2019_prem_df_for_ml_10_v2.txt', 'rb') as myFile:
+with open(f'../prem_clean_fixtures_and_dataframes/{df_10_saved_name}', 'rb') as myFile:
     df_ml_10 = pickle.load(myFile)
 
 
@@ -66,7 +70,7 @@ y_5 = df_ml_5['Team Result Indicator']
 x_train, x_test, y_train, y_test = train_test_split(x_10, y_10, test_size=0.2)
 
 #instantiate the MLP classifier, and fit to the data
-clf = MLPClassifier(hidden_layer_sizes=(18, 12), activation='logistic', random_state=0, max_iter=2000)
+clf = MLPClassifier(hidden_layer_sizes=(18, 12), activation='logistic', random_state=0, max_iter=5000)
 clf.fit(x_train, y_train)
 
 #printing the cross-validation accuracy score
@@ -76,59 +80,81 @@ print('Cross-Validation Accuracy Score ML10: ', cv_score_av, '%\n')
 
 #--------------------------------- GRID SEARCH --------------------------------
 
-#creating the list of tuples to test the length of hidden layers
-hidden_layer_test = []
-for i in range(6,20,2):
-    a = list(range(6,30,4))
-    b = [i] * len(a)    
-    c = list(zip(a, b))
-    hidden_layer_test.extend(c)    
+if grid_search:
+    #creating the list of tuples to test the length of hidden layers
+    hidden_layer_test = []
+    for i in range(6,20,2):
+        a = list(range(6,30,4))
+        b = [i] * len(a)    
+        c = list(zip(a, b))
+        hidden_layer_test.extend(c)    
+        
+    param_grid_grad = [{'hidden_layer_sizes':hidden_layer_test}]
     
-param_grid_grad = [{'hidden_layer_sizes':hidden_layer_test}]
+    #mlp gridsearch 
+    grid_search_grad = GridSearchCV(clf, param_grid_grad, cv=5, scoring = 'accuracy', return_train_score = True)
+    grid_search_grad.fit(x_10, y_10)
 
-#mlp gridsearch 
-grid_search_grad = GridSearchCV(clf, param_grid_grad, cv=5, scoring = 'accuracy', return_train_score = True)
-grid_search_grad.fit(x_10, y_10)
+    #Output best Cross Validation score and parameters from grid search
+    print('\n', 'Gradient Best Params: ' , grid_search_grad.best_params_)
+    print('Gradient Best Score: ' , grid_search_grad.best_score_ , '\n')
 
-#Output best Cross Validation score and parameters from grid search
-print('\n', 'Gradient Best Params: ' , grid_search_grad.best_params_)
-print('Gradient Best Score: ' , grid_search_grad.best_score_ , '\n')
-
-print(grid_search_grad.cv_results_['mean_test_score'])
+    print(grid_search_grad.cv_results_['mean_test_score'])
 
 
 # ---------- PLOTTING THE DATA ----------
 
-#populating df with x, y, z data
-matrix_plot_data = pd.DataFrame({})
-matrix_plot_data['x'] = list(zip(*hidden_layer_test))[0]
-matrix_plot_data['y'] = list(zip(*hidden_layer_test))[1]
-matrix_plot_data['z'] = grid_search_grad.cv_results_['mean_test_score']
+if grid_search:
+    #populating df with x, y, z data
+    matrix_plot_data = pd.DataFrame({})
+    matrix_plot_data['x'] = list(zip(*hidden_layer_test))[0]
+    matrix_plot_data['y'] = list(zip(*hidden_layer_test))[1]
+    matrix_plot_data['z'] = grid_search_grad.cv_results_['mean_test_score']
+    
+    #transforming z list into matrix format
+    Z = matrix_plot_data.pivot_table(index='x', columns='y', values='z').T.values
+    
+    #getting x and y axis
+    X_unique = np.sort(matrix_plot_data.x.unique())
+    Y_unique = np.sort(matrix_plot_data.y.unique())
+    
+    #instantiating figure and plotting heatmap with seaborn
+    fig, ax = plt.subplots()
+    im = sns.heatmap(Z, annot=True,  linewidths=.5)
+    
+    #labelling x and y and title
+    ax.set_xticklabels(X_unique)
+    ax.set_yticklabels(Y_unique)
+    ax.set(xlabel='Hidden Layer 1 Length',
+            ylabel='Hidden Layer 2 Length');
+    fig.suptitle('Cross Val Accuracy', y=0.95, fontsize=16, fontweight='bold');
+    
+    if save_grid_search_fig:
+        fig.savefig('figures/testing_hidden_layer_lengths.png')    
 
-#transforming z list into matrix format
-Z = matrix_plot_data.pivot_table(index='x', columns='y', values='z').T.values
 
-#getting x and y axis
-X_unique = np.sort(matrix_plot_data.x.unique())
-Y_unique = np.sort(matrix_plot_data.y.unique())
+#--------------------------------- FINAL MODEL --------------------------------
 
-#instantiating figure and plotting heatmap with seaborn
-fig, ax = plt.subplots()
-im = sns.heatmap(Z, annot=True,  linewidths=.5)
+#in this section we will take the learnings from the hyperparameter testing above and train a final model using 100% of the data. This model may then be used for predictions going forward.
 
-#labelling x and y and title
-ax.set_xticklabels(X_unique)
-ax.set_yticklabels(Y_unique)
-ax.set(xlabel='Hidden Layer 1 Length',
-        ylabel='Hidden Layer 2 Length');
-fig.suptitle('Cross Val Accuracy', y=0.95, fontsize=16, fontweight='bold');
+if create_final_model:
+    
+    #intantiating and training the df_5 network
+    ml_5_mlp = MLPClassifier(hidden_layer_sizes=(18, 12), activation='relu', random_state=0, max_iter=5000)
+    ml_5_mlp.fit(x_5, y_5)
+    
+    #intantiating and training the df_10 network
+    ml_10_mlp = MLPClassifier(hidden_layer_sizes=(18, 12), activation='relu', random_state=0, max_iter=5000)
+    ml_10_mlp.fit(x_10, y_10)
+    
+    with open('ml_models/mlp_model_5.pk1', 'wb') as myFile:
+        pickle.dump(ml_5_mlp, myFile)
 
-#saving figure
-fig.savefig('figures/testing_hidden_layer_lengths.png')    
+    with open('ml_models/mlp_model_10.pk1', 'wb') as myFile:
+        pickle.dump(ml_10_mlp, myFile)
 
 
 # ----------------------------------- END -------------------------------------
 
 print('\n', 'Script runtime:', round(((time.time()-start)/60), 2), 'minutes')
 print(' ----------------- END ----------------- \n')
-
