11407a7fe3e5224049e3d50f25aef3ed6e4e83e1
Matt Haythornthwaite <m.haythornthwaite@yahoo.co.uk>
2020-09-22 18:30:21 +0100

    Testing Alternative Features

--- ml_model_build_ensemble_model/ensemble_model.py
+++ ml_model_build_ensemble_model/ensemble_model.py
@@ -18,19 +18,18 @@ sys.path.append(dirname(realpath(__file__)) + sep + pardir + sep)
 import time
 start=time.time()
 
-import pandas as pd
 import pickle
 import numpy as np
-import math
 from ml_functions.data_processing import scale_df
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import VotingClassifier
-from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_val_predict
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
+
+#------------------------------- ML MODEL BUILD -------------------------------
 
 with open('../prem_clean_fixtures_and_dataframes/2019_2020_prem_df_for_ml_10_v2.txt', 'rb') as myFile:
     df_ml_10 = pickle.load(myFile)
-    
     
 #scaling dataframe to make all features to have zero mean and unit vector.
 df_ml_10 = scale_df(df_ml_10, list(range(14)), [14,15,16])
@@ -41,6 +40,8 @@ y_10 = df_ml_10['Team Result Indicator']
 X_train, X_test, y_train, y_test = train_test_split(x_10, y_10, test_size=0.2)
 
 
+# ---------- LOADING MODELS ----------
+
 clf_rf = pickle.load(open('../ml_model_build_random_forest/ml_models/random_forest_model_10.pk1', 'rb'))
 
 clf_svm = pickle.load(open('../ml_model_build_support_vector_machine/ml_models/svm_model_10.pk1', 'rb'))
@@ -49,6 +50,8 @@ clf_knn = pickle.load(open('../ml_model_build_nearest_neighbor/ml_models/knn_mod
 
 clf_mlp = pickle.load(open('../ml_model_build_deep_learning/ml_models/mlp_model_10.pk1', 'rb'))
 
+
+# ---------- ENSEMBLE MODELLING ----------
 
 #create a dictionary of our models
 estimators=[('rf', clf_rf), ('svm', clf_svm), ('knn', clf_knn), ('mlp', clf_mlp)]
@@ -61,13 +64,11 @@ ensemble = VotingClassifier(estimators, voting='hard')
 #fit model to training data
 ensemble.fit(X_train, y_train)
 
-
 #cross validation
 skf = StratifiedKFold(n_splits=5, shuffle=True)
 
 cv_score_av = round(np.mean(cross_val_score(ensemble, x_10, y_10, cv=skf))*100,1)
 print('Cross-Validation Accuracy Score ML10: ', cv_score_av, '%\n')
-
 
 
 # ----------------------------------- END -------------------------------------
---
+++ ml_model_build_random_forest/random_forest_feature_testing.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 18:08:41 2020
+
+@author: mhayt
+"""
+
+
+print('\n\n ---------------- START ---------------- \n')
+
+#-------------------------------- API-FOOTBALL --------------------------------
+
+#!/usr/bin/python
+from os.path import dirname, realpath, sep, pardir
+import sys
+sys.path.append(dirname(realpath(__file__)) + sep + pardir + sep)
+
+import time
+start=time.time()
+
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from ml_functions.data_processing import scale_df
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix, accuracy_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score, cross_val_predict
+
+
+#------------------------------- INPUT VARIABLES ------------------------------
+
+fixtures_saved_name = '2019_2020_premier_league_fixtures_df.csv'
+
+
+#---------------------- ALTERNATIVE FEATURE ENGINEERING -----------------------
+
+fixtures_clean = pd.read_csv(f'../prem_clean_fixtures_and_dataframes/{fixtures_saved_name}')
+
+with open('../prem_clean_fixtures_and_dataframes/2019_2020_prem_df_for_ml_10_v2.txt', 'rb') as myFile:
+    df_ml_10 = pickle.load(myFile)
+
+alt_df = pd.DataFrame({})  
+
+alt_df['Shots Diff'] = df_ml_10['Team Av Shots Diff'] - df_ml_10['Opponent Av Shots Diff'] 
+alt_df['Shots Box Diff'] = df_ml_10['Team Av Shots Inside Box Diff'] - df_ml_10['Opponent Av Shots Inside Box Diff'] 
+alt_df['Corners Diff'] = df_ml_10['Team Av Corners Diff'] - df_ml_10['Opponent Av Corners Diff'] 
+alt_df['Possession Diff'] = df_ml_10['Team Av Possession Diff'] - df_ml_10['Opponent Av Possession Diff']
+alt_df['Pass Diff'] = df_ml_10['Team Av Pass Accuracy Diff'] - df_ml_10['Opponent Av Pass Accuracy Diff']
+alt_df['Goal Diff'] = df_ml_10['Team Av Goal Diff'] - df_ml_10['Opponent Av Goal Diff']
+alt_df['Fixture ID'] = df_ml_10['Fixture ID']
+alt_df['Team Result Indicator'] = df_ml_10['Team Result Indicator']
+alt_df['Opponent Result Indicator'] = df_ml_10['Opponent Result Indicator']
+
+x_10 = alt_df.drop(['Fixture ID', 'Team Result Indicator', 'Opponent Result Indicator'], axis=1)
+y_10 = alt_df['Team Result Indicator']
+
+
+#------------------------------- ML MODEL BUILD -------------------------------
+
+def rand_forest_train(df, print_result=True, print_result_label=''):
+
+    #create features matrix
+    x = df.drop(['Fixture ID', 'Team Result Indicator', 'Opponent Result Indicator'], axis=1)
+    y = df['Team Result Indicator']
+    
+    #instantiate the random forest class
+    clf = RandomForestClassifier(max_depth=4, max_features=4, n_estimators=120)
+    
+    #split into training data and test data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    
+    #train the model
+    clf.fit(x_train, y_train)
+    
+    if print_result:
+        print(print_result_label)
+        #training data
+        train_data_score = round(clf.score(x_train, y_train) * 100, 1)
+        print(f'Training data score = {train_data_score}%')
+        
+        #test data
+        test_data_score = round(clf.score(x_test, y_test) * 100, 1)
+        print(f'Test data score = {test_data_score}% \n')
+    
+    return clf, x_train, x_test, y_train, y_test
+
+
+ml_10_rand_forest, x10_train, x10_test, y10_train, y10_test = rand_forest_train(alt_df, print_result_label='DF_ML_10')
+
+
+# ---------- CROSS VALIDATION ----------
+
+skf = StratifiedKFold(n_splits=5, shuffle=True)
+
+cv_score_av = round(np.mean(cross_val_score(ml_10_rand_forest, x_10, y_10, cv=skf))*100,1)
+print('Cross-Validation Accuracy Score ML10: ', cv_score_av, '%\n')
+
+
+# ---------- FEATURE IMPORTANCE ----------
+
+fi_ml_10 = pd.DataFrame({'feature': list(x10_train.columns),
+                         'importance': ml_10_rand_forest.feature_importances_}).sort_values('importance', ascending = False)
+
+
+# ----------------------------------- END -------------------------------------
+
+print('\n', 'Script runtime:', round(((time.time()-start)/60), 2), 'minutes')
+print(' ----------------- END ----------------- \n')
